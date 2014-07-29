f7ea9d61dbd8a9065756270d3d51097b554711dc
Nguyễn Phan Hải <hainp2604@gmail.com>
2014-07-29 09:39:31 +0700

    Prototyping homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery.min
 //= require jquery_ujs
+//= require jquery.slick
 //= require foundation
 //= require turbolinks
 //= require_tree .
-
 $(function(){ $(document).foundation(); });
--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,2 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function () {
+    $('.banners .slides').slick({
+        'arrows': false,
+        'autoplay': true
+    });
+
+    $('.lookbook .slides').slick({
+        'slidesToShow': 3,
+        'slidesToScroll': 2
+    });
+});

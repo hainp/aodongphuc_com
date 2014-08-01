627abfd7ee7fe1f2d0e7fea7956a9e53a4c42bf6
Nguyễn Phan Hải <hainp2604@gmail.com>
2014-08-01 09:49:31 +0700

    Render social channels in footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :set_items_for_menu
+  before_filter :set_items_for_menu, :set_social_channels
   
   def set_items_for_menu
     @menus = Menu.all
+  end
+  
+  def set_social_channels
+    @channels = SocialChannel.all
   end
 end

959553fd20256bd810f9de4d5a5cd580dc009e06
Nguyễn Phan Hải <hainp2604@gmail.com>
2014-08-01 10:41:12 +0700

    Read web info from config file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :set_items_for_menu, :set_social_channels
-  
+
   def set_items_for_menu
     @menus = Menu.all
   end
-  
+
   def set_social_channels
     @channels = SocialChannel.all
   end

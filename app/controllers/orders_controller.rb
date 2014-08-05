9af8391d4df9086e24e4612c31141f2d7974eb90
Nguyễn Phan Hải <hainp2604@gmail.com>
2014-08-05 17:55:21 +0700

    Finish order page prototype

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,23 @@
 class OrdersController < ApplicationController
+
+  # GET /orders/new
+  def new
+    @orders = Order.new
+  end
+  
+  # POST /orders
+  # POST /orders.json
+  def create
+    @order = Order.new(orders_params)
+
+    respond_to do |format|
+      if @order.save
+        format.html { redirect_to @order, notice: 'Order was successfully created.' }
+        format.json { render :show, status: :created, location: @order }
+      else
+        format.html { render :new }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end

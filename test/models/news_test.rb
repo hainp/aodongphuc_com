ebbd06487112eaa942d6a38fa2b527cfd071a38d
Nguyễn Phan Hải <hainp2604@gmail.com>
2014-08-06 21:34:13 +0700

    More news' tests

--- test/models/news_test.rb
+++ test/models/news_test.rb
@@ -4,4 +4,15 @@ class NewsTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "select 2 news" do
+    @news_list = News.order(created_at: :desc).limit(2)
+    assert_equal 2, @news_list.size
+  end
+
+  test "select 2 news continuosly" do
+    @news_list = News.order(created_at: :desc).limit(2)
+    @news_list += News.order(created_at: :desc).limit(2).offset(2)
+    assert_equal 4, @news_list.size
+  end
 end

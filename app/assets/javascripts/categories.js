8d3c742ab2c88739fd1bbcfd014a92811638e045
Nguyễn Phan Hải <hainp2604@gmail.com>
2014-08-12 07:46:23 +0700

    Display products in category

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,2 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+    $('.products .slide-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: 'slide-nav',
+        arrows: false
+    });
+
+    $('.products .slide-nav').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.slide-for',
+        focusOnSelect: true
+    });
+});

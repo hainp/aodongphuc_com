2ae8a593db165726a2a1392fafc9ad9269823797
Nguyễn Phan Hải <hainp2604@gmail.com>
2014-08-19 10:38:22 +0700

    Limit products at index page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
   def index    
     # Use array.in_group_of(number_of_columns)
     @categories = Category.all
-    @products = Product.all
+    @products = Product.first(8)
     @banners = Banners.all
     @buttons = Button.all
   end
